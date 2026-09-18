cdf2862d5a42bc8d5e16b6b09dd50ee14eb44058
Лука Важник <Tyuiu.VazhnikLN@outlook.com>
2026-09-18 18:00:20 +0500

    Task0

---
+++ Tyuiu.VazhnikLN.Sprint1.Task0.V19.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.VazhnikLN.Sprint1.Task0.V19.Lib;
+namespace Tyuiu.VazhnikLN.Sprint1.Task0.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiveTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(24, res);
+        }
+    }
+}
---
+++ Tyuiu.VazhnikLN.Sprint1.Task0.V19/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Tyuiu.VazhnikLN.Sprint1.Task0.V19.Lib;
+namespace Tyuiu.VazhnikLN.Sprint1.Task0.V19
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Важник Л. Н. | ПКТб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Важние Лука Николаевич | ПКТб-26-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет значение выражения                *");
+            Console.WriteLine("* 20 - (2 * 2 - 8) и печатает результат на экране                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 20 - (2 * 2 - 8)                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate());
+            Console.ReadLine();
+
+        }
+    }
+}
