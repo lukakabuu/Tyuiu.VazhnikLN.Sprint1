150211563a527cf5835d877ab22bf5c3611aafc9
Лука Важник <Tyuiu.VazhnikLN@outlook.com>
2026-09-18 18:15:53 +0500

    Task0

--- Tyuiu.VazhnikLN.Sprint1.Task0.V15.Lib/DataService.cs
+++ Tyuiu.VazhnikLN.Sprint1.Task0.V15.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.VazhnikLN.Sprint1.Task0.V15.Lib
 {
-    public class DataService : ISprint1Task0V19
+    public class DataService : ISprint1Task0V15
     {
         public double Calculate()
         {
