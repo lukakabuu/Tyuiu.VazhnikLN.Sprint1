dfafe6d5070575b159063e1d8804def02e44e576
Лука Важник <Tyuiu.VazhnikLN@outlook.com>
2026-09-20 11:41:34 +0500

    Task6

---
+++ Tyuiu.VazhnikLN.Sprint1.Task6.V2.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.VazhnikLN.Sprint1.Task6.V2.Lib;
+namespace Tyuiu.VazhnikLN.Sprint1.Task6.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ExpressionValid()
+        {
+            DataService ds = new DataService();
+            string x = "Hello, world";
+            var res = ds.CheckHello(x);
+            Assert.AreEqual(true, res);
+        }
+    }
+}
---
+++ Tyuiu.VazhnikLN.Sprint1.Task6.V2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Tyuiu.VazhnikLN.Sprint1.Task6.V2.Lib;
+namespace Tyuiu.VazhnikLN.Sprint1.Task6.V2
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Важник Л. Н. | ПКТб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Важник Лука Николаевич | ПКТб-26-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст. Проверить, есть          *");
+            Console.WriteLine("* и в строке слово Hello.                                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Вводит пользователь                                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите ваш текст:");
+            string s = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.CheckHello(s));
+            Console.ReadLine();
+
+
+        }
+    }
+}
